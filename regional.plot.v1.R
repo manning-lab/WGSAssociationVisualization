7a6110cb6331ed683ef195ea17cb0d755a60fd9f
tmajaria <tmajaria@broadinstitute.org>
2019-08-13 12:16:20 -0400

    ld info to regional plot, other files for regional plotting

--- regional.plot.v1.R
+++ regional.plot.v1.R
@@ -128,7 +128,10 @@ makeBedTrack <- function(bed_data,
   return(bed.track)
 }
 
-makeVariantTrack <- function(variant_data, chr_column, pos_column, y_column, 
+makeVariantTrack <- function(variant_data, chr_column, pos_column, y_column,
+                              marker_column = NULL,
+                              ld_data = NULL,
+                              ld_ref = NULL,
                               genome_build = "hg19", 
                               should_log = T,
                               horizontal_value = 5e-8,
@@ -138,14 +141,89 @@ makeVariantTrack <- function(variant_data, chr_column, pos_column, y_column,
                               background_frame = T,
                               point_color = "#252525",
                               title = ""){
+  
   # log the data if we need to
   if (should_log){
     variant_data[,y_column] <- -log10(variant_data[,y_column])
     horizontal_value <- -log10(horizontal_value)
   }
 
-  # generate the variant track
-  variant.track <- DataTrack(
+  # merge with LD information if we need to
+  if (!is.null(marker_column) & !is.null(ld_data) & !is.null(ld_ref)){
+    # get to right format
+    ld_data <- t(ld_data[,2:ncol(ld_data)])
+    ld_data <- data.frame(
+      marker = row.names(ld_data),
+      r = ld_data[,1],
+      stringsAsFactors = F)
+
+    # first make sure the markername column is correct in the ld data file
+    names(ld_data)[1] <- marker_column
+
+    # make sure that we have the same sep character in the marker names
+    ld_data[,marker_column] <- gsub("[[:punct:]]", "_", ld_data[,marker_column])
+    ld_ref <- gsub("[[:punct:]]", "_", ld_ref)
+    variant_data[,marker_column] <- gsub("[[:punct:]]", "_", variant_data[,marker_column])
+
+    # then merge
+    variant_data <- merge(variant_data,
+                          ld_data,
+                          by = marker_column,
+                          all.x = T)
+
+    # column number with ld information
+    ld_col <- ncol(variant_data)
+
+    # generate the colors 
+    variant_data$color <- "#B8B8B8"
+    v.na <- variant_data[is.na(variant_data[,ld_col]),]
+    v.notna <- variant_data[!is.na(variant_data[,ld_col]),]
+    v.notna[v.notna[,ld_col] >= 0, "color"] <- "#357EBD" 
+    v.notna[v.notna[,ld_col] >= 0.2, "color"] <- "#46B8DA" 
+    v.notna[v.notna[,ld_col] >= 0.4, "color"] <- "#5CB85C" 
+    v.notna[v.notna[,ld_col] >= 0.6, "color"] <- "#EEA236" 
+    v.notna[v.notna[,ld_col] >= 0.8, "color"] <- "#D43F3A"
+    
+    variant_data <- rbind(v.na, v.notna)
+    variant_data[variant_data[,marker_column] == ld_ref, "color"] <- "#9632B8"
+    
+    colors <- c("#B8B8B8", "#357EBD", "#46B8DA", "#5CB85C", "#EEA236", "#D43F3A", "#9632B8")
+    
+    # get y bounds for plot
+    ylims <- c(0, max(variant_data$pvalue))
+
+    # generate the variant track list, to combine later
+    variant.tracks <- list()
+    for (val in colors){
+      cur_data <- variant_data[variant_data$color == val,]
+      if (nrow(cur_data) == 0) next
+      variant.tracks[[length(variant.tracks) + 1]] <- DataTrack(
+        data = cur_data[,y_column], 
+        start = cur_data[,pos_column], 
+        end = cur_data[,pos_column], 
+        chromosome = cur_data[,chr_column], 
+        genome = genome_build, 
+        name = title, 
+        background.title = background_color,
+        col.frame = background_color,
+        frame = background_frame,
+        baseline = horizontal_value,
+        col.baseline = horizontal_color,
+        lty.baseline = horizontal_style,
+        col = cur_data$color[1],
+        ylim = ylims)
+    }
+
+    variant.track <- OverlayTrack(variant.tracks,
+      background.title = background_color,
+      col.frame = background_color,
+      frame = background_frame,
+    )
+    
+  } else {
+    print("No LD information found.")
+    # generate the variant track
+    variant.track <- DataTrack(
       data = variant_data[,y_column], 
       start = variant_data[,pos_column], 
       end = variant_data[,pos_column], 
@@ -158,14 +236,17 @@ makeVariantTrack <- function(variant_data, chr_column, pos_column, y_column,
       baseline = horizontal_value,
       col.baseline = horizontal_color,
       lty.baseline = horizontal_style,
-      col = point_color
-    )
+      col = point_color)
+  }
   return(variant.track)
 }
 
 makePlot <- function(chr, start, end, variant_data, variant_chr_column, variant_pos_column, variant_y_column,
+                        variant_marker_column = NULL,
+                        variant_ld_data = NULL,
+                        variant_ld_ref = NULL,
                         genome_build = "hg19",
-                        variant_should_log = F,
+                        variant_should_log = T,
                         variant_horizontal_value = 5e-8,
                         variant_horizontal_color = "red",
                         variant_horizontal_style = "dashed",
@@ -183,10 +264,14 @@ makePlot <- function(chr, start, end, variant_data, variant_chr_column, variant_
                         bed_frame = T){
   
   # make the data track for variant statistics
+  print(min(variant_data[,variant_y_column]))
   variant.track <- makeVariantTrack(variant_data, 
                                     variant_chr_column, 
                                     variant_pos_column, 
                                     variant_y_column, 
+                                    variant_marker_column,
+                                    variant_ld_data,
+                                    variant_ld_ref,
                                     genome_build, 
                                     variant_should_log, 
                                     variant_horizontal_value,
@@ -251,18 +336,23 @@ makePlot <- function(chr, start, end, variant_data, variant_chr_column, variant_
 
 
 # testing inputs
-data.dir <- "/Users/tmajaria/Dropbox (Partners HealthCare)/"
-variant.file <- paste0(data.dir, "Whradjbmi.giant-ukbb.meta-analysis.combined.23May2018.HapMap2_only.TM_proc.chr1.tsv.gz")
+data.dir <- "/Users/tmajaria/Documents/projects/WGSAssociationVisualization/"
+variant.file <- paste0(data.dir, "1kg-t2d.all.assoc.aug12.txt.gz")
+ld.file <- paste0(data.dir, "1kg-t2d.chr20_60.9M-61.1M.ld.csv")
 bed.file <- paste0(data.dir, "Islets.chromatinStates.bed")
 
-chr <- 1
-start <- 219000000
-end <- 220000000
+chr <- 20
+start <- 60900000
+end <- 61100000
 variant_data <- fread(variant.file, data.table = F, stringsAsFactors = F)
 variant_data <- variant_data[!is.na(variant_data$chr),]
+variant_data$MarkerName <- sub("chr", "", variant_data$MarkerName)
 variant_chr_column = "chr"
 variant_pos_column = "pos"
-variant_y_column = "P"
+variant_y_column = "pvalue"
+variant_marker_column = "MarkerName"
+variant_ld_data <- fread(ld.file, data.table = F, stringsAsFactors = F)
+variant_ld_ref = "20-61000005-A-G"
 genome_build = "hg19"
 variant_should_log = T
 variant_horizontal_value = 5e-8
@@ -283,6 +373,9 @@ bed_background_colors = c("#3182bd")
 bed_frame = T
 
 makePlot(chr, start, end, variant_data, variant_chr_column, variant_pos_column, variant_y_column,
+         variant_marker_column,
+         variant_ld_data,
+         variant_ld_ref,
          genome_build,
          variant_should_log,
          variant_horizontal_value,
