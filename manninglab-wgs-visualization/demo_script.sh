68a43953aeca9972eada772cbb5f0f5b138c4846
shruti-par <parpattedar.s@husky.neu.edu>
2019-08-22 11:30:43 -0400

    Copy files

--- manninglab-wgs-visualization/demo_script.sh
+++ manninglab-wgs-visualization/demo_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -rf /srv/shiny-server /tmp
+#cp -rf /srv/shiny-server /tmp
 #cp /srv/shiny-server/1kg-t2d.all.assoc.aug12.txt.gz.tbi /tmp
 #cp /srv/shiny-server/1kg-t2d.chr20_60.9M-61.1M.ld.csv /tmp
 
--- manninglab-wgs-visualization/login_script.sh
+++ manninglab-wgs-visualization/login_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set timeout -1
-cp -r /srv/shiny-serve/ /tmp/
+# cp -r /srv/shiny-server/ /tmp/
 # cp /srv/shiny-server/1kg-t2d.all.assoc.aug12.txt.gz.tbi /tmp
 # cp /srv/shiny-server/1kg-t2d.chr20_60.9M-61.1M.ld.csv /tmp
 
