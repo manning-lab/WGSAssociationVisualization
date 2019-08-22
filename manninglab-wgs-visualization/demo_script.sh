b945624ecaa7bc9507f87a9d95cb5d8312a4c64c
shruti-par <parpattedar.s@husky.neu.edu>
2019-08-22 11:56:12 -0400

    Removed extra lines

--- manninglab-wgs-visualization/demo_script.sh
+++ manninglab-wgs-visualization/demo_script.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-#cp -rf /srv/shiny-server /tmp
-#cp /srv/shiny-server/1kg-t2d.all.assoc.aug12.txt.gz.tbi /tmp
-#cp /srv/shiny-server/1kg-t2d.chr20_60.9M-61.1M.ld.csv /tmp
-
 chmod a+x /usr/bin/shiny-server.sh
 source /usr/bin/shiny-server.sh
--- manninglab-wgs-visualization/login_script.sh
+++ manninglab-wgs-visualization/login_script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set timeout -1
-# cp -r /srv/shiny-server/ /tmp/
-# cp /srv/shiny-server/1kg-t2d.all.assoc.aug12.txt.gz.tbi /tmp
-# cp /srv/shiny-server/1kg-t2d.chr20_60.9M-61.1M.ld.csv /tmp
 
 gcloud auth application-default login
 read REPLY
