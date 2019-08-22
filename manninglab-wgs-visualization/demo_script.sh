d6238b945495f8982ac351b57abcfa28921227d6
shruti-par <parpattedar.s@husky.neu.edu>
2019-08-22 11:16:50 -0400

    Files copy to tmp

--- manninglab-wgs-visualization/demo_script.sh
+++ manninglab-wgs-visualization/demo_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp /srv/shiny-server/1kg-t2d.all.assoc.aug12.txt.gz /tmp
-cp /srv/shiny-server/1kg-t2d.all.assoc.aug12.txt.gz.tbi /tmp
-cp /srv/shiny-server/1kg-t2d.chr20_60.9M-61.1M.ld.csv /tmp
+cp -rf /srv/shiny-server /tmp
+#cp /srv/shiny-server/1kg-t2d.all.assoc.aug12.txt.gz.tbi /tmp
+#cp /srv/shiny-server/1kg-t2d.chr20_60.9M-61.1M.ld.csv /tmp
 
 chmod a+x /usr/bin/shiny-server.sh
 source /usr/bin/shiny-server.sh
--- manninglab-wgs-visualization/login_script.sh
+++ manninglab-wgs-visualization/login_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set timeout -1
-cp /srv/shiny-server/1kg-t2d.all.assoc.aug12.txt.gz /tmp
-cp /srv/shiny-server/1kg-t2d.all.assoc.aug12.txt.gz.tbi /tmp
-cp /srv/shiny-server/1kg-t2d.chr20_60.9M-61.1M.ld.csv /tmp
+cp -r /srv/shiny-serve/ /tmp/
+# cp /srv/shiny-server/1kg-t2d.all.assoc.aug12.txt.gz.tbi /tmp
+# cp /srv/shiny-server/1kg-t2d.chr20_60.9M-61.1M.ld.csv /tmp
 
 gcloud auth application-default login
 read REPLY
