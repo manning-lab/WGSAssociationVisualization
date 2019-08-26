27e31ea8d16530ce02117d609f8e7f6d3d40a8cb
shruti-par <parpattedar.s@husky.neu.edu>
2019-08-26 10:50:55 -0400

    Navigation instruction added

--- manninglab-wgs-visualization/demo_script.sh
+++ manninglab-wgs-visualization/demo_script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "Navigate to http://127.0.0.1:3838/ on your web browser"
+
 chmod a+x /usr/bin/shiny-server.sh
 source /usr/bin/shiny-server.sh
--- manninglab-wgs-visualization/login_script.sh
+++ manninglab-wgs-visualization/login_script.sh
@@ -7,5 +7,7 @@ read REPLY
 cp /root/.config/gcloud/application_default_credentials.json /tmp
 chmod a+rx /tmp/application_default_credentials.json
 
+echo "Navigate to http://127.0.0.1:3838/ on your web browser"
+
 chmod a+x /usr/bin/shiny-server.sh
 source /usr/bin/shiny-server.sh
