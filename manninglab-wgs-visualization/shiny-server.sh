a2c03f7e4233b055a185b762ebcbbcc79f78cea0
shruti-par <parpattedar.s@husky.neu.edu>
2019-08-28 08:38:10 -0400

    Shiny server logs displyed only in the log file

--- manninglab-wgs-visualization/shiny-server.sh
+++ manninglab-wgs-visualization/shiny-server.sh
@@ -4,11 +4,5 @@
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
 
-if [ "$APPLICATION_LOGS_TO_STDOUT" != "false" ];
-then
-	# push the "real" application logs to stdout with xtail in detached mode
-	exec xtail /var/log/shiny-server/ &
-fi
-
-# start shiny server
+# Start shiny server
 exec shiny-server >> /var/log/shiny-server.log 2>&1
